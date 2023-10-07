bfdf450dc52661be3dd69e03fc0e14c70c64a540
Jason Lin <jasonlinfl@gmail.com>
2023-10-07 18:59:44 -0400

    complete discord command shell

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from dotenv import dotenv_values
-from discord import app_commands
 from discord.ext import commands
+from discord import app_commands
 
 token = dotenv_values(".env")["TOKEN"]
 guild_id = dotenv_values(".env")["GUILD"]
@@ -9,7 +9,6 @@ guild_id = dotenv_values(".env")["GUILD"]
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='.', intents=intents)
-
 
 @bot.event
 async def on_ready():
@@ -19,11 +18,30 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
-    print(message.content)
 
-@bot.tree.command(name='generatebackground', description = "Generate a background for your dnd character", guild=discord.Object(guild_id))
-async def generatebackground(interaction: discord.Interaction):
-    await interaction.response.send_message("Hello World")
+@bot.tree.command(name='generatestory', 
+                  description = "Generate a background for your dnd character choose ur options", 
+                  guild=discord.Object(guild_id))
+@app_commands.describe(character_name="What's your character's name?")
+@app_commands.describe(race="ex. Dwarf, Dragonborn, Human, etc")
+@app_commands.describe(char_class="ex. Barbarian, Bard, Cleric, etc.")
+@app_commands.describe(background="ex. Noble, Soldier, Urchin, etc.")
+@app_commands.describe(alignment="ex. Lawful Good, Neutral, Urchin, etc.")
+@app_commands.describe(personality_traits="(use 'and' to include two personality traits) ex. Curious, Cheerful and Cautious, Sarcastic, etc.")
+@app_commands.describe(ideals='ex. Justice (Believes in fairness and law), Knowledge (Seeks to discover and learn as much as possible), etc.')
+@app_commands.describe(bonds="(use 'and' to include more than one bond) ex. Family and Comrades (Cherishes loyalty to family and the bonds formed with friends), Mentor (Feels a debt to a benefactor, mentor, or organization), etc.")
+@app_commands.describe(flaws='ex. Greedy (Overly possessive or desirous of wealth or possessions), Fearful (Possesses a deep-seated fear or phobia), etc.')
+async def generatestory(interaction: discord.Interaction, 
+                        character_name: str, 
+                        race: str, 
+                        char_class: str, 
+                        background: str,
+                        alignment: str = None,
+                        personality_traits: str = None,
+                        ideals: str = None,
+                        bonds: str = None,
+                        flaws: str = None):
+    await interaction.response.send_message(race)
 
 
 bot.run(token)
